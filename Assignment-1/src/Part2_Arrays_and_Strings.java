a028294292b2e7812214e8f76a0ee5e7062105a1
Alina Ying <alina.ying@gmail.com>
2019-06-28 13:32:26 -0400

    Added optimized version of Part 2, question 2.

--- Assignment-1/src/Part2_Arrays_and_Strings.java
+++ Assignment-1/src/Part2_Arrays_and_Strings.java
@@ -18,7 +18,55 @@ public class Part2_Arrays_and_Strings {
 	 * This function takes an array of integers and a target integer to which the array
 	 * elements must sum. It returns an array of all pairs of integers from the input
 	 * array whose sum equals the target.
+	 *
+	 * Order does not matter: [2,4], is the same as [4,2]
+	 * No repeats: [2,2,4] -> result: [2,4]
 	 */
+	public static ArrayList<ArrayList<Integer>> pairsThatEqualSum_optimized(int[] inputArray, int targetSum){
+		ArrayList<ArrayList<Integer>> result = new ArrayList<>();
+		HashMap<Integer, Integer> number_to_count = new HashMap<>();
+
+		for(int number: inputArray){
+			if(number_to_count.get(number) == null){
+				number_to_count.put(number, 1);
+			}
+			else{
+				number_to_count.put(number, number_to_count.get(number) + 1);
+			}
+		}
+
+		for(Map.Entry<Integer, Integer> entry: number_to_count.entrySet()){
+			Integer key = entry.getKey();
+			Integer count = entry.getValue();
+
+			Integer opposite = targetSum - key;
+			if(key.equals(opposite)){
+				Integer num_of_iterations = count/2;
+				for(int i = 0; i < num_of_iterations; i++){
+					ArrayList<Integer> pair = new ArrayList<>();
+					pair.add(key);
+					pair.add(opposite);
+					result.add(pair);
+				}
+			}
+			else {
+				while (count > 0) {
+					Integer opposite_count = number_to_count.get(opposite);
+					if (opposite_count != null && opposite_count > 0) {
+						ArrayList<Integer> pair = new ArrayList<>();
+						pair.add(key);
+						pair.add(opposite);
+						result.add(pair);
+						number_to_count.put(opposite, opposite_count - 1);
+						number_to_count.put(key, count - 1);
+					}
+					count--;
+				}
+			}
+		}
+		return result;
+	}
+
 	public static ArrayList<ArrayList<Integer>> pairsThatEqualSum(int[] inputArray, int targetSum){
 		ArrayList<Integer> array_to_check= new ArrayList<Integer>();
 		for(int i: inputArray) {
--- Assignment-1/tests/Part2_Arrays_and_Strings_Test.java
+++ Assignment-1/tests/Part2_Arrays_and_Strings_Test.java
@@ -1,41 +1,40 @@
-import org.junit.Assert;
 import org.junit.Test;
-
 import java.util.ArrayList;
+import static org.junit.Assert.*;
 
-class Part2_Arrays_and_Strings_Test {
+public class Part2_Arrays_and_Strings_Test {
     @Test
-    void testIsStringPermutation() {
-        Assert.assertTrue(Part2_Arrays_and_Strings.isStringPermutation("Listen", "Silent"));
-        Assert.assertFalse(Part2_Arrays_and_Strings.isStringPermutation("happiness", "smile"));
-        Assert.assertFalse(Part2_Arrays_and_Strings.isStringPermutation("", "-1"));
-        Assert.assertTrue(Part2_Arrays_and_Strings.isStringPermutation("345", "435"));
+    public void testIsStringPermutation() {
+        assertTrue(Part2_Arrays_and_Strings.isStringPermutation("Listen", "Silent"));
+        assertFalse(Part2_Arrays_and_Strings.isStringPermutation("happiness", "smile"));
+        assertFalse(Part2_Arrays_and_Strings.isStringPermutation("", "-1"));
+        assertTrue(Part2_Arrays_and_Strings.isStringPermutation("345", "435"));
     }
 
     @Test
-    void testPairsThatEqualSum() {
+    public void testPairsThatEqualSum() {
         int[] arr = new int[]{1, 7, 3, 18, 1, 2, 2};
-        /*
-        int[][] correct_result1 =  [[1, 3], [2, 2]];
-        int[][] correct_result2 = [[]];
-        */
 
         ArrayList<ArrayList<Integer>> target_4 = Part2_Arrays_and_Strings.pairsThatEqualSum(arr, 4);
         ArrayList<ArrayList<Integer>> target_0 = Part2_Arrays_and_Strings.pairsThatEqualSum(arr, 0);
 
         assert target_0.size() == 0;
         assert target_4.size() == 2;
+    }
 
-        /*
-        for(int i = 0; i < correct_result1.length; i++){
-            for(int j = 0; j < correct_result1[i].length; j++){
-                Assert.assertEquals(correct_result1[i][j], target_4.get(i).get(j));
-            }
-        }
-        for(int i = 0; i < correct_result2.length; i++){
-            for(int j = 0; j < correct_result2[i].length; j++){
-                Assert.assertEquals(correct_result1[i][j], target_0.get(i).get(j));
-            }
-        }*/
+    @Test
+    public void testPairsThatEqualSum_optimized() {
+        int[] arr = new int[]{1, 7, 3, 18, 1, 2, 2};
+        int[] arr2 = new int[]{2, 4, 3, 1, 5};
+
+        ArrayList<ArrayList<Integer>> target_4 = Part2_Arrays_and_Strings.pairsThatEqualSum_optimized(arr, 4);
+        ArrayList<ArrayList<Integer>> target_0 = Part2_Arrays_and_Strings.pairsThatEqualSum_optimized(arr, 0);
+        ArrayList<ArrayList<Integer>> target_6 = Part2_Arrays_and_Strings.pairsThatEqualSum_optimized(arr2, 6);
+        System.out.println(target_6);
+        System.out.println(target_4);
+        System.out.println(target_0);
+
+        assert target_0.size() == 0;
+        assert target_4.size() == 2;
     }
 }
