673871bb456b678058594ea5703a4e95109d68b6
Alina Ying <alina.ying@gmail.com>
2019-08-08 16:31:08 -0400

    Finalized solution for Ex 2: level-by-level printing and Ex 3: printing depth. TBD: more comprehensive testing

--- Assignment-2/src/Part1_OrganizationStructure.py
+++ Assignment-2/src/Part1_OrganizationStructure.py
@@ -18,6 +18,7 @@ class Employee:
     def __str__(self):
         return "Name: {name}, Title: {title}".format(name=self.name, title=self.title)
 
+
 """
     An organization class, which only understands hierarchy levels.
 """
@@ -25,12 +26,12 @@ class OrganizationStructure:
     ceo: Employee = None    # head employee
     employees_by_levels: collections.deque = None    # queue with all employees in level order
     current_position: int = None    # current position in queue
-    count: int = None
+    level_count: int = None         # global count variable for collecting employees in queue
 
     def __init__(self, highest_authority: Employee):
         self.ceo = highest_authority
         self.current_position = 0
-        self.count = 1
+        self.level_count = 1
         self.employees_by_levels = collections.deque([{self.ceo: 1}])
 
     """
@@ -55,9 +56,44 @@ class OrganizationStructure:
     Name: K, Title: Sales Intern
     """
     def printLevelByLevel(self):
-        self.current_position = 0
         self.collectEmployeesInQueue(self.ceo, 2)
 
+        while self.employees_by_levels:
+            popped_node = self.employees_by_levels.popleft()
+            popped_employee = list(popped_node.keys())[0]
+            popped_level = popped_node.get(popped_employee)
+
+            if popped_level > self.level_count:
+                print()
+                self.level_count += 1
+
+            print(popped_employee)
+
+        # clean up: sets current position to 0 again
+        self.current_position = 0
+
+
+    """
+    [Trees - Ex3] Exercise: Printing the number of levels
+    Implement a method called printNumLevels() for the class OrganizationStructure that prints the number of levels.
+    """
+    def printNumLevels(self):
+        self.collectEmployeesInQueue(self.ceo, 2)
+        popped_node = self.employees_by_levels.pop()
+        popped_employee = list(popped_node.keys())[0]
+        popped_level = popped_node.get(popped_employee)
+        print(popped_level)
+
+        # clean up: makes sure queue is empty.
+        while self.employees_by_levels:
+            self.employees_by_levels.popleft()
+
+        # clean up: sets current position to 0 again
+        self.current_position = 0
+
+    """
+        Add all employees into employees_by_levels queue.
+    """
     def collectEmployeesInQueue(self, root_employee: Employee, current_level: int):
         if root_employee.directReports is not None:
             for underling in root_employee.directReports:
@@ -68,21 +104,3 @@ class OrganizationStructure:
             current_node: dict = list(self.employees_by_levels)[self.current_position]
             current_employee = list(current_node.keys())[0]
             self.collectEmployeesInQueue(current_employee, current_node.get(current_employee) + 1)
-
-        popped_node = self.employees_by_levels.popleft()
-        popped_employee = list(popped_node.keys())[0]
-        popped_level = popped_node.get(popped_employee)
-
-        if popped_level > self.count:
-            print()
-            self.count += 1
-
-        print(popped_employee)
-
-
-    """
-    [Trees - Ex3] Exercise: Printing the number of levels
-    Implement a method called printNumLevels() for the class OrganizationStructure that prints the number of levels.
-    """
-    def printNumLevels(self):
-        pass
--- Assignment-2/tests/Part1_OrganizationStructure_Test.py
+++ Assignment-2/tests/Part1_OrganizationStructure_Test.py
@@ -23,3 +23,23 @@ class Part1_OrganizationStructure_Test(TestCase):
 
         test_structure = OrganizationStructure(ceo)
         test_structure.printLevelByLevel()
+
+    def test_printNumLevels(self):
+        sales_intern = Employee("K", "Sales Intern", None)
+
+        sales_rep = Employee("J", "Sales Representative", [sales_intern])
+        engineer_f = Employee("F", "Engineer", None)
+        engineer_g = Employee("G", "Engineer", None)
+        engineer_h = Employee("H", "Engineer", None)
+
+        director = Employee("I", "Director", [sales_rep])
+        manager_d = Employee("D", "Manager", [engineer_f, engineer_g, engineer_h])
+        manager_e = Employee("E", "Manager", None)
+
+        cfo = Employee("B", "CFO", [director])
+        cto = Employee("C", "CTO", [manager_d, manager_e])
+
+        ceo = Employee("A", "CEO", [cfo, cto])
+
+        test_structure = OrganizationStructure(ceo)
+        test_structure.printNumLevels()
